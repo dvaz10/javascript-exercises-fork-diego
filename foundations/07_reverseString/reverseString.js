bf6cfee2b3d34374c268e3b74f7f8a0c9204a8dc
Diego <227276562+dvaz10@users.noreply.github.com>
2026-06-26 14:59:42 -0600

    solve 07_reverseString. Use for loop and indexes.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = ""
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverse += string[i];
+
+    }
+    return reverse;
+
 
 };
-
+console.log(reverseString("Hola soy diego. el num 1"))
 // Do not edit below this line
 module.exports = reverseString;
