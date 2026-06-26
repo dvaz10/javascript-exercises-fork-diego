823a44e0dbd8c1265627c54388c7febf3d377af9
Diego <227276562+dvaz10@users.noreply.github.com>
2026-06-26 10:17:55 -0600

    Solve 06_repeatString. Made repeat function. ERROR if repeat < 0

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, repeat) {
+    let result = ""
+    if (repeat >= 0) {
+        for (let i = 0; i < repeat; i++) {
+            result += word
+        }
+        return result;
+    }
+    else {
+        return "ERROR";
+    }
+    
 
 };
-
+console.log(repeatString('hey', 0))
+console.log(repeatString('hello', -1))
 // Do not edit below this line
 module.exports = repeatString;
+
