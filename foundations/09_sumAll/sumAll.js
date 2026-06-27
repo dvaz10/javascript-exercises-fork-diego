c6bf7bc3907e2edefbeb571dee724626801dd5e3
Diego <227276562+dvaz10@users.noreply.github.com>
2026-06-26 21:49:35 -0600

    solve 09_sumAll. create array and then reduce(). No need to create array. Loop for += i, sum all numbers.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let arr = [];
+    if ((Number.isInteger(start) && start >= 0) && (Number.isInteger(end) && end >= 0)) {
+        if (start <= end ) {
+            for (let i = start; i <= end; i++ )
+                arr.push(i)
+        }
+        else {
+            for (let i = end; i <= start; i++ )
+                arr.push(i)           
+        }
+         
+    }   
+    else {
+        return "ERROR";
+    }
+    return arr.reduce((a,b) => a + b)
+    
+   
+    
 
 };
+
+console.log(sumAll(1,4000000))
 
 // Do not edit below this line
 module.exports = sumAll;
