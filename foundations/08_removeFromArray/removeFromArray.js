f9d61670c440d55cd15eea1cd6604ad536c5170e
Diego <227276562+dvaz10@users.noreply.github.com>
2026-06-26 21:22:30 -0600

    solve 08_removeFromArray. Use findIndex(), slice() first. filter() better. forEach() also option

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+/*
+const removeFromArray = function(array, ...values) {
+    for (const value of values) {
+        let index = array.findIndex(arr => arr === value)
+        array.splice(index, 1)
+    }
+    return array;
 };
-
+*/
+const removeFromArray = function(array, ...values) {
+    return array.filter(arr => !values.includes(arr))
+        
+};
+console.log((removeFromArray([1, 2, 2, "he"], 1,"2",4)))
 // Do not edit below this line
 module.exports = removeFromArray;
