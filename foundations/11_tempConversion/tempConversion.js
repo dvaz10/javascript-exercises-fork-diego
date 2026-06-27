43df3ef50b7e18c4922635dd918a0626f5c750d1
Diego <227276562+dvaz10@users.noreply.github.com>
2026-06-26 22:34:49 -0600

    solve 11_tempConversion. used Math.round(x * 10)/10 to round to 1 decimal.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9)
+  celsius = Math.round(celsius * 10) / 10
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * 9/5) + 32
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit;
 };
+
+console.log(convertToCelsius(32))
+console.log(convertToFahrenheit(73.2))
 
 // Do not edit below this line
 module.exports = {
