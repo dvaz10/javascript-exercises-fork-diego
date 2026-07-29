6f89e0f5a8ebb7906f4646bfa1a4b78b7f3b548e
Diego <227276562+dvaz10@users.noreply.github.com>
2026-07-28 21:54:07 -0600

    made function that takes an array of numbers and add all the numbers together

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(x, y) {
 	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return Number(numbers.reduce((sum, value) => sum + value, 0));
 };
 
 const multiply = function() {
