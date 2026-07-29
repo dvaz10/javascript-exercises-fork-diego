5308170aba7d03a3fa8f9c22926abdf93fe5587b
Diego <227276562+dvaz10@users.noreply.github.com>
2026-07-29 09:48:05 -0600

    create function that determines if a string is a palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+    const original = string
+    .toLowerCase()
+    .split("")
+    .filter(character => characters.includes(character))
+    .join("");
+
+
+    const palindrome = original
+    .split("")
+    .reverse()
+    .join("");
+    
+
+    console.log(original)
+    console.log(palindrome)
+
+    return palindrome === original;
 
 };
-
+console.log(palindromes("race car!"))
 // Do not edit below this line
 module.exports = palindromes;
