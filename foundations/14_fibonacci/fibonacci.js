84c0ecb617a27447087f5be29194361e4746fcc8
Diego <227276562+dvaz10@users.noreply.github.com>
2026-07-29 17:40:23 -0600

    Create function that returns a specific member of the Fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [0,1];
+    let value = 1;
+    for (let i = 0; i < num; i++) {
+        value += fib[i];
+        fib.push(value);
+
+    }
+    console.log(fib)
+    return fib[num];
 
 };
-
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
