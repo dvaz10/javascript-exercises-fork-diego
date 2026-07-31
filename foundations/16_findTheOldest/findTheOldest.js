8a79f0b8c90e156920fdb48359b10ee232e6a674
Diego <227276562+dvaz10@users.noreply.github.com>
2026-07-30 21:32:24 -0600

    made function return index 0 of array created. Cleaned code.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -15,41 +15,11 @@ const findTheOldest = function(array) {
         };
 })
 
-    console.log(peopleAge);
-
     const older = peopleAge.sort((a,b) => b.age - a.age);
 
-    return older;
+    return older[0];
 
 };
-
-
-
-
-
-
-
-
-    const people = [
-      {
-        name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-        
-      },
-      {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-      },
-    ]
-
-console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
