05c132007d25a3767f84d0c8c743228ab9cfb3aa
Diego <227276562+dvaz10@users.noreply.github.com>
2026-07-30 18:13:55 -0600

    create function that creates array of object with name and age

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const peopleAge = array.map(item => {
+
+        let age = 0
+        if (Object.keys(item).includes("yearOfDeath")) {
+            age = item.yearOfDeath - item.yearOfBirth
+        }
+        else {
+            age = (new Date()).getFullYear() - item.yearOfBirth
+        }
+    
+       return {
+            name: item.name,
+            age
+        }
+})
+
+return peopleAge
 
 };
+
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
