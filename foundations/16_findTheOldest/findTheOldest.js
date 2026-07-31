c8af8741ac5413f550ac963e9bf767fbca3ebb32
Diego <227276562+dvaz10@users.noreply.github.com>
2026-07-30 19:54:08 -0600

    create function that sort people objects by age (oldest to youngest)

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -3,26 +3,38 @@ const findTheOldest = function(array) {
 
         let age = 0
         if (Object.keys(item).includes("yearOfDeath")) {
-            age = item.yearOfDeath - item.yearOfBirth
+            age = item.yearOfDeath - item.yearOfBirth;
         }
         else {
-            age = (new Date()).getFullYear() - item.yearOfBirth
-        }
+            age = (new Date()).getFullYear() - item.yearOfBirth;
+        };
     
        return {
             name: item.name,
             age
-        }
+        };
 })
 
-return peopleAge
+    console.log(peopleAge);
+
+    const older = peopleAge.sort((a,b) => b.age - a.age);
+
+    return older;
 
 };
+
+
+
+
+
+
+
 
     const people = [
       {
         name: "Carly",
         yearOfBirth: 1942,
+        yearOfDeath: 1970,
         
       },
       {
